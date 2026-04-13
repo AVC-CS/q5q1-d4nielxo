6ecc51731874905e5759b5105e5351319503273c
d4nielxo <dhaddad3@students.avc.edu>
2026-04-13 13:07:30 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main()
 {
     int n;
-    // TODO: ask user for input
+    cin >> n;
 
-    // TODO: outer loop for each row
-
-        // TODO: inner loop to print letters for this row
-
-            // TODO: print the letter for this column
-
-        // TODO: print newline after each row
-
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= i; j++) {
+            char letter = 'A' + j;
+            cout << letter;
+            if (j < i) {
+                cout << " ";
+            }
+        }
+        if (i < n - 1) {
+            cout << endl;
+        }
+    }
 
     return 0;
 }
